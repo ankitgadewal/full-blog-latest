280193609df25f7d27c98bd0edd0bf74b820c0e2
ankit <ankitgadewal@hotmail.com>
2020-05-09 11:27:45 -1200

    Initial Commit

---
+++ app1/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='index'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/new-post', views.PostCreateView.as_view(), name='new-post'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('search', views.search, name='search'),
+    path('about/', views.about, name='about'),
+]
---
+++ app1/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+# Create your views here.
+# def index(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/index.html', {'posts':posts})
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/user-posts.html'
+    # ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/new-post.html'
+    fields = ['title', 'content']
+    # success_url = '/' instead of this we created a function in models which redirect a user on the post that he created
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/new-post.html'
+    fields = ['title', 'content']
+    # success_url = '/' instead of this we created a function in models which redirect a user on the post that he created
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView, SuccessMessageMixin):
+    model = Post
+    template_name = 'blog/post-delete.html'
+    success_url = '/'
+    success_message = 'Post Deleted!!'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def search(request):
+    query = request.GET['query']
+    if len(query) > 50 or len(query)<1:
+        posts = []
+    else:
+        posts = Post.objects.filter(title__icontains=query).union(Post.objects.filter(content__icontains=query))
+        length = len(posts)
+    return render(request, 'blog/search.html', {'posts': posts, 'query': query, 'length':length})
